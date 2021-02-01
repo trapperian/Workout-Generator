6fa1df40b356367035a2ce644308f322d6aaa680
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-01 06:44:45 -0700

    Create diceWorkoutGenerator.py
    a basic, two die random bodyweight workout generator, no equipment needed, answer question, dice rolls for you and you've got the routine

---
+++ diceWorkoutGenerator.py
@@ -0,0 +1,20 @@
+# random dice workout generator
+# uses two 'dice' to randomly generate a basic workout without equipment.
+
+#exercises used are squat, pushup, jumping jacks, squat thrust, mountain climber, prone swimmers
+#set/reps are actually times each numbe represents corresponding amount of minutes so, 1 = 1 minute, 2 = 2 minutes, etc.
+
+
+import random
+
+#TODO: finish dice roll function with two dice, one for exercises one for time
+def diceRoll():
+    if answer == 'y':
+
+
+
+print('Have you already taken a half hour walk today, gotten 8 hours of sleep or more and drank some water? ("y" or "n")')
+answer = input()
+
+    exercise = random.randint(1, 6)
+
