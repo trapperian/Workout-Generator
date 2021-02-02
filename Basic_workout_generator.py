c241960a77dd98b4a813bd03111820b2afa0651f
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-02 06:35:18 -0700

    update basic generator started deckofcards
    Used MU tidy function on basic_workout_generator to clean up code.
    Opened file for a deckOfCardsGenerator

--- Basic_workout_generator.py
+++ Basic_workout_generator.py
@@ -1,14 +1,23 @@
-#this is a basic workout generator, input according to requests and it will give you a recommended workout for the day.
+# this is a basic workout generator, input according to requests and it will give you a recommended workout for the day.
 
-while True: #since it seems that Al Sweigart recommends this for starting all of his programs
-    print('Did you do a workout of some sort or other yesterday? (Please use "yes" or "no")')
+while (
+    True
+):  # since it seems that Al Sweigart recommends this for starting all of his programs
+    print(
+        'Did you do a workout of some sort or other yesterday? (Please use "yes" or "no")'
+    )
     prior_wkt = input()
-    if prior_wkt == 'yes':
-        print('Go for a walk for at least half an hour.')
-        print('Workout is done for the day, go home, eat your vegetables, drink water, get 8 hours of sleep.')
-    elif prior_wkt == 'no':
-        print("This workout recommendation is for an absolute beginner, it's pretty hard to get hurt on this one but standard disclaimers apply of getting doctor approval and  you assume all liability for damages.  This is a coding project, not a fitness recommendation.")
-        print("""
+    if prior_wkt == "yes":
+        print("Go for a walk for at least half an hour.")
+        print(
+            "Workout is done for the day, go home, eat your vegetables, drink water, get 8 hours of sleep."
+        )
+    elif prior_wkt == "no":
+        print(
+            "This workout recommendation is for an absolute beginner, it's pretty hard to get hurt on this one but standard disclaimers apply of getting doctor approval and  you assume all liability for damages.  This is a coding project, not a fitness recommendation."
+        )
+        print(
+            """
         First and foremost, breathing, you're going to spend 2 minutes doing what is called, \"Crocodile Breathing\" lay on your stomach, put your forehead on your hands and breathe, focusing on your stomach/below the ribcage doing the work of breathing.  Further information: https://www.functionalmovement.com/Exercises/823/crocodile_breathing_with_ankle_weights \n
         Second, roll over onto your back, put your feet up on a wall or chair so that your knees are at roughly a 90 degree angle, 2 more minutes of breathing, focusing on the same action as before.  This is how important breathing is.
         Third, that rolling motion you did to go from prone to supine, do that back and forth, across both sides leading with arms and/or legs for 2 minutes, tomorrow your obliques will want to have a discussion with you.
@@ -18,7 +27,7 @@ while True: #since it seems that Al Sweigart recommends this for starting all of
         Seventh, crab crawl for two minutes.
         After a brief rest, do 'get back ups.' Lie on the floor and then stand up, repeat following positions and rules here: https://www.youtube.com/watch?v=o0_DoicHg2E
         Finally, go take a half hour walk.  That is a total of an hourish of a workout, you've improved your breathing, mobility, neural connections, basic strength, cardio and worked on some of the major killers of U.S. adults daily.
-        Go home, drink water, eat vegetables and get 8 hours of sleep.  Continue this workout until you are feeling pretty sure of yourself and feeling 'well put together.' """)
+        Go home, drink water, eat vegetables and get 8 hours of sleep.  Continue this workout until you are feeling pretty sure of yourself and feeling 'well put together.' """
+        )
     else:
         print("Please use the specific word's 'yes' or 'no'.")
-
---
+++ deckOfCardsGenerator.py
@@ -0,0 +1,6 @@
+# deck of cards workout generator
+# upon starting deck of cards generator will start
+# each suit has assigned exercise
+# each card has assigned rep count
+
+#dictionary for cards?
