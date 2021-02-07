af287e299e34e0fad81a840457a4996c2756e9f1
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-07 16:59:02 -0700

    Added comments to functionality used tidy function

--- deckOfCardsGenerator.py
+++ deckOfCardsGenerator.py
@@ -9,38 +9,38 @@ class Card:
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
+
     def show(self):
         print("{} of {}".format(self.value, self.suit))
 
 
 class Deck:
-    def __init__(self):
+    def __init__(self):  # initiating Deck
         self.cards = []
         self.build()
 
-    def build(self):
+    def build(self):  # actually making the card deck
         for suit in ["Spades", "Hearts", "Diamonds", "Clubs"]:
             for v in range(1, 15):
                 self.cards.append(Card(suit, v))
 
-    def show(self):
+    def show(self):  # showing the deck entirely
         for card in self.cards:
             card.show()
 
-    def shuffle(self):
+    def shuffle(self):  # shuffling the deck
         import random
-        for i in range(len(self.cards)-1):
+
+        for i in range(len(self.cards) - 1, 0, -1):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
+    def pop(self):  # removing card from deck
+        return self.cards.pop()
 
 
-deck = Deck()
-deck.shuffle()
-deck.show()
+
 # deck of cards as classes to add/pop off etc to work down whole deck.
 # upon starting deck of cards generator will start
 # asks for 4 exercises by user
 # each card has assigned rep count
-
-
