0793f32921017235f59491ec7401c373c1af008a
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-07 15:14:56 -0700

    Add Card and Deck classes
    Starting to build objects to shuffle and show

--- deckOfCardsGenerator.py
+++ deckOfCardsGenerator.py
@@ -1,6 +1,18 @@
+#! python3
+class Card:
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
+
+
+class Deck:
+    def __init__(self):
 # deck of cards workout generator
+# deck of cards as classes to add/pop off etc to work down whole deck.
 # upon starting deck of cards generator will start
-# each suit has assigned exercise
+# asks for 4 exercises by user
 # each card has assigned rep count
 
-#dictionary for cards?
+
