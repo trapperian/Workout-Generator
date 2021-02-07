584be09f2c4b93eb225418a5cc4af4c61639d03e
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-07 15:52:41 -0700

    Update and check Card class

--- deckOfCardsGenerator.py
+++ deckOfCardsGenerator.py
@@ -1,4 +1,8 @@
 #! python3
+# deck of cards worout generator
+# will eventually take input of four exercises by user
+# default 4 if no input are: pushup, squat, lunge, single leg deadlift
+# bonus jokers are 100 jumping jacks ;)
 class Card:
     def __init__(self, suit, val):
         self.suit = suit
@@ -9,10 +13,12 @@ class Card:
 
 class Deck:
     def __init__(self):
-# deck of cards workout generator
+        pass
+
 # deck of cards as classes to add/pop off etc to work down whole deck.
 # upon starting deck of cards generator will start
 # asks for 4 exercises by user
 # each card has assigned rep count
-
+card = Card('spade', 5)
+card.show()
 
