fa7b0888f8f694d0caf1d2caa89e5c7771e65ed6
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-10 06:15:16 -0700

    Brainstorming other projects

---
+++ ConvictConditioningWorkoutBuilder.py
@@ -0,0 +1,4 @@
+# This program will use the Paul wade books by the program
+# Starting with new blood program, working up to the 3 days a week and adding in extra work at approcpriate times
+# Will have warm up sets and work sets, adding 1 rep per set per day and asking whether accomplished.  If yes add rep if not drop down a rep and start again
+# show graphs of work too like that little endurance app does.
---
+++ recipeWebScrapperCompiler.py
@@ -0,0 +1,3 @@
+#scrape websites for recipe ingredients, compile ingredients list for shopping
+#add to calendar of meals/when to defrost meat or other extras?
+#align ingredients with seasonality/repeat uses for weekly meal plans
